f88bda2586ffb46ed0d01e418f0cf9121aa719d5
Marco <marcovenicio1515@gmail.com>
2021-04-29 19:53:50 -0400

    commit

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/style.css">
